872e380a5bcdefd1a5e8c8ca5ac058ad658f8ffe
Appi <iphonedev@appi.com.br>
2018-07-17 12:30:20 -0300

    Initial Commit

---
+++ HelloMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMVVM
+//
+//  Created by Fernando on 17/07/2018.
+//  Copyright © 2018 Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
